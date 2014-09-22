694ac0dd6828d10bc04bdd90bb312c7fc0bcd445
Greg Trahair <greg.trahair@gmail.com>
2014-09-22 11:02:34 +0200

    Removed node specifics to allow full hiera node configuration

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,16 +1,4 @@
 # site.pp
-node 'default' {
-    class { 'epel': }
-}
 
-node 'test-puppetmaster-001.puppetbootstrap.local' inherits default {
-    class { 'puppetdb': }
-    class { 'puppet::master':
-        storeconfigs => true,
-        version      => '3.5.1-1.el6',
-        autosign     => true,
-    }
-    class { 'puppet::agent':
-        puppet_server => 'test-puppetmaster-001.puppetbootstrap.local',
-    }
-}
+# hiera class lookups.
+hiera_include('classes')
