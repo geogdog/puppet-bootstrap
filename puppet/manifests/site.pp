66d5c389992eaace81ff578c5d0174881a5d27f4
Greg Trahair <greg.trahair@gmail.com>
2014-10-02 16:33:57 +0200

    Fixup the main puppet bootstrap

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -2,3 +2,12 @@
 
 # hiera class lookups.
 hiera_include('classes')
+
+# include jenkins plugins from hiera
+create_resources(jenkins::plugin, hiera_hash('jenkins_plugins', {}))
+
+# hosts can be included in hiera too
+create_resources(host, hiera_hash('hosts', {}))
+
+# packages can be included in hiera too
+create_resources(package, hiera_hash('extra_packages', {}))
